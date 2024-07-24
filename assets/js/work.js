31d4ea31c3ba428be74f740cef2b8257e33d0c41
Russell S. Lebo <datainkio@users.noreply.github.com>
2024-07-24 10:04:45 -0400

    Better handing of alternating grid pattern.

--- assets/js/work.js
+++ assets/js/work.js
@@ -4,29 +4,28 @@
  * - Assign scrollTo behaviors to buttons
  */
 document.addEventListener("DOMContentLoaded", (event) => {
+
   let dur = .25; // base value for animation duration (in seconds)
   let y_delta = 35; // vertical origin of elements
-  let pos = "-=.1"; // offset (in seconds) of animation relative to the previous
+  let pos = "-=50%"; // offset (in seconds) of animation relative to the previous
   // gsap.registerPlugin(ScrollTrigger, ScrollToPlugin);
 
   /** INITIALIZE VIEW */
   const cards = gsap.utils.toArray("a.card.project");
   cards.forEach(card => {
-    if (!card.classList.contains("featured")) {
       card.style.opacity = 0;
-      card.style.display = "none";
-    }
+      card.classList.add("hidden");
   });
 
 
-  /** TITLE */
-  let welcome = gsap.timeline({});
-
+  /** WELCOME SEQUENCE */
+  let welcome = gsap.timeline();
   welcome.add(gsap.from("h1", {duration: dur, y:y_delta, autoAlpha: 0, ease: "sine.inOut", delay: .5}));
-  welcome.add(gsap.from(".abstract", {duration: dur, autoAlpha: 0, ease: "sine.inOut",}));
+  welcome.add(gsap.from(".abstract", {duration: dur, autoAlpha: 0, ease: "sine.inOut",}), pos);
   // welcome.add(gsap.from('.card.project.featured', {autoAlpha: 0, duration: dur, ease: "sine.inOut"}), pos);
   welcome.add(gsap.from('.navbar-end', {autoAlpha: 0, duration: dur, ease: "sine.inOut"}), pos);
 
+  // CONTROLS
   const btns = gsap.utils.toArray('.navbar-start li');
   btns.forEach(btn => {
     welcome.add(gsap.from(btn, {
@@ -35,14 +34,15 @@ document.addEventListener("DOMContentLoaded", (event) => {
       duration: dur
     }), pos);
   });
-  /**
-  const cards = document.querySelectorAll('#view_featured .card')
-  gsap.from(cards, {
-      autoAlpha: 0,
-      y: y_delta, 
-      duration: dur
-    });
-    */
+
+  // SHOW FEATURED PROJECTS
+  const featured = gsap.utils.toArray("a.card.project.featured");
+  featured.forEach(card => {
+    card.classList.remove("hidden");
+    card.classList.add("current");
+    welcome.add(gsap.to(card, {opacity: 1, y:-y_delta, duration: dur*.76, ease: "sine.inOut"}), pos);
+  });
+  applyAlternatingStyles();
 
   /** 
    * CATEGORY SELECTION
@@ -64,24 +64,68 @@ document.addEventListener("DOMContentLoaded", (event) => {
           }
         }
         // Update the project previews
-        const cards = gsap.utils.toArray("a.card.project");
+        const outro = gsap.timeline({onComplete: clearCards});
+        const intro = gsap.timeline({onStart: showSelected, onStartParams: [cat]});
         cards.forEach(card => {
+          // clear all visible projects
+          if (!card.classList.contains("hidden")) {
+            outro.add(gsap.to(card, {opacity: 0, y:y_delta, duration: dur*.75, ease: "sine.inOut"}), pos);
+          }
+          
           if (card.classList.contains(cat.id)) {
-            // display the project
-            gsap.to(card, {opacity: 1, display: "block", delay: .5, duration: dur, ease: "sine.inOut"});
-          } else {
-            // hide the project
-            gsap.to(card, {opacity: 0, display: "none", duration: dur, ease: "sine.inOut"});
+            // display projects belonging to the selected category
+            intro.add(gsap.to(card, {opacity: 1, y:-y_delta, duration: dur*.75, ease: "sine.inOut"}), pos);
           }
         })
+        const transition = gsap.timeline({});
+        // intro.pause();
+        transition.add(outro);
+        transition.add(intro);
+        transition.play();
       })
 
-     // Update the label with title of the selected category
+     // Update the button label with title of the selected category
      label.innerHTML = cat.innerHTML;
-     // Update the button to reflect its active state
-     
-      // gsap.to(window, {duration: 1, scrollTo:{y:cat.id, offsetY: 72}});
     });
   })
 
+  function clearCards() {
+    console.log("clearCards");
+    cards.forEach((card, index) => {
+      card.classList.add("hidden");
+      card.classList.remove("current");
+      card.classList.remove("even-column");
+    })
+  }
+
+  function showSelected(cat) {
+    let projects = gsap.utils.toArray("." + cat.id);
+    console.log("showSelected");
+    projects.forEach(project => {
+      project.classList.remove("hidden");
+      project.classList.add("current");
+    });
+    applyAlternatingStyles();
+  };
+
+  function applyAlternatingStyles() {
+    const grid = document.getElementById('projects');
+    const cols = window.getComputedStyle(grid).getPropertyValue('grid-template-columns').split(' ').length;
+    const current = gsap.utils.toArray(".project.current");
+    console.log(current.length + " cards in " + cols + " columns");
+    current.forEach((card, index) => {
+      const colIndex = (index % cols) + 1; // Get the 1-based column index
+      if ((cols > 1) && (colIndex % 2 !== 0)) {
+        card.classList.remove('even-column');
+      } else {
+        card.classList.add('even-column');
+      }
+    });
+
+  }
+  window.addEventListener('resize', applyAlternatingStyles);
+  let portrait = window.matchMedia("(orientation: portrait)");
+  portrait.addEventListener("change", function(e) {
+      applyAlternatingStyles();
+  })
 });
